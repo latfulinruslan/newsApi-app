731affe032ebed51902d56f5bb04cbbb04eee857
Ruslan Latfulin <latfulinr@gmail.com>
2019-08-27 18:43:04 +0300

    Added Article struct

---
+++ newsApp/newsApp/Model/Article.swift
@@ -0,0 +1,19 @@
+//
+//  Article.swift
+//  newsApp
+//
+//  Created by Ruslan Latfulin on 8/27/19.
+//  Copyright © 2019 Ruslan Latfulin. All rights reserved.
+//
+
+import Foundation
+
+struct Article {
+    var author: String
+    var title: String
+    var description: String
+    var publishedAt: String
+    var urlToImage: String
+    var url: String
+    var content: String
+}
