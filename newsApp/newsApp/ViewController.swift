e96941931703fe01f38fe35e0789a553da662e6e
Ruslan Latfulin <latfulinr@gmail.com>
2019-08-27 17:58:42 +0300

    Project init

---
+++ newsApp/newsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  newsApp
+//
+//  Created by Ruslan Latfulin on 8/27/19.
+//  Copyright © 2019 Ruslan Latfulin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
